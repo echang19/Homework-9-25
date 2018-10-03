7d4484fc6d36b32547acd99df5772f8aa1d65e9f
echang19 <43009998+echang19@users.noreply.github.com>
2018-10-03 12:46:13 -0400

    Add files via upload

---
+++ Hw103.py
@@ -0,0 +1,15 @@
+'''
+Created on Oct 3, 2018
+
+@author: Evan A Chang
+'''
+#Name
+fname=input('Please enter your first name:')
+lname=input('Please enter your last name:')
+
+#Initials
+C1=fname[0]
+C2=lname[0]
+lfname=len(fname)
+llname=len(lname)
+ffname=len
