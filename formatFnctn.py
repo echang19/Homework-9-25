e06e318df824bfc6deb4cccd4cbeb29aba6e11ba
echang19 <43009998+echang19@users.noreply.github.com>
2018-10-17 12:45:09 -0400

    Add files via upload

---
+++ formatFnctn.py
@@ -0,0 +1,19 @@
+'''
+Created on Oct 17, 2018
+
+@author: Evan Chang
+'''
+name1=input("Please enter your full name")
+acnum1=float(input("Please enter your bank account number")
+chec1=input("Please enter the type of the account")
+B1=float(input("Please enter your balance in the account")
+name2=input("Please enter your full name")
+acnum2=float(input("Please enter your bank account number")
+chec2=input("Please enter the type of the account")
+B2=float(input("Please enter your balance in the account")      
+name3=input("Please enter your full name")
+acnum3=float(input("Please enter your bank account number")
+chec3=input("Please enter the type of the account")
+B3=float(input("Please enter your balance in the account")  
+print("All Accounts:")   
+print("Acc Num:", format(acnum1,'>011')+format("Name:",'8s'), format(name1,) )   
