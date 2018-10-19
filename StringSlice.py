2d6f618fa37582da3c59b85709579aef828612f3
echang19 <43009998+echang19@users.noreply.github.com>
2018-10-19 12:03:50 -0400

    Add files via upload

---
+++ StringSlice.py
@@ -0,0 +1,3 @@
+stg=input("Please enter any string")
+slic=float(input('Please enter the index where you would like to start slicing')
+slicen=('please enter an index where you like to end the index')
