0b538bbc878b929583f23df56e5ab0a05fa6ce5d
echang19 <43009998+echang19@users.noreply.github.com>
2018-11-16 12:55:38 -0500

    Add files via upload

---
+++ MontyHall.py
@@ -0,0 +1,13 @@
+'''
+Created on Nov 16, 2018
+
+@author: Evan A. Chang
+'''
+
+import random
+car= random.randint(1,3)
+
+guess=int(input("please pick a door you think has the Car (1, 2 , or 3)"))
+
+print("The car was behind door #",car) 
+print("you guessed door #",guess)
