3b2cd1453c75435119d86304db35610b3a51e736
echang19 <43009998+echang19@users.noreply.github.com>
2018-12-05 11:58:57 -0500

    Add files via upload

---
+++ L00psday2practicehw.py
@@ -0,0 +1,11 @@
+'''
+Created on Dec 5, 2018
+
+@author: Evan A. Chang
+
+Whileloop calculator
+'''
+num=int(input("Please enter a number that is not zero"))
+while num != 0:
+    num=int(input("Please enter a number that is not zero"))
+    
