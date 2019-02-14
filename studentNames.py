3455bbd6320e3b588eb6898a9bccd18baf31d58d
echang19 <43009998+echang19@users.noreply.github.com>
2019-02-14 12:43:42 -0500

    Add files via upload

---
+++ studentNames.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 14, 2019
+
+@author: Evan A. Chang
+
+Student names
+'''
+
+def main():
+    
+    
+def studentNames(list):
+    print("when you are done your list type done")
+    list=[student.lower]
+    while student.lower != "done"
+    student.lower.append=input("please enter the  name of the student")
+    
