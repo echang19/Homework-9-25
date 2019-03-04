983eff38fa43d64febef1ddb1f4fb5ef01250d58
echang19 <43009998+echang19@users.noreply.github.com>
2019-03-04 12:52:49 -0500

    Add files via upload

---
+++ Pg403n7.py
@@ -0,0 +1,18 @@
+'''
+Created on Feb 28, 2019
+
+@author: Evan A. Chang
+Driver's License
+'''
+def main():
+    cor=0
+    Correct=["A", 'C', 'A', 'A', 'D', 'B', 'C', 'A', 'C', 'B', 'A', 'D', 'C', 'A', 'D', 'C', 'B', 'B', 'D', 'A']
+    Student=[]
+    studenta=open(Test.txt, 'r')
+    while Student != "done":
+        Student.append=input("Please enter with capital A, B, C, D:")
+    
+    if cor < 15:
+        
+
+
