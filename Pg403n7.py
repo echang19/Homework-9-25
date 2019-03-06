c4e2bd8b6debd95b1db1fb324fd52bddca3947d1
echang19 <43009998+echang19@users.noreply.github.com>
2019-03-06 12:44:53 -0500

    Add files via upload

--- Pg403n7.py
+++ Pg403n7.py
@@ -6,13 +6,24 @@ Driver's License
 '''
 def main():
     cor=0
-    Correct=["A", 'C', 'A', 'A', 'D', 'B', 'C', 'A', 'C', 'B', 'A', 'D', 'C', 'A', 'D', 'C', 'B', 'B', 'D', 'A']
-    Student=[]
-    studenta=open(Test.txt, 'r')
-    while Student != "done":
-        Student.append=input("Please enter with capital A, B, C, D:")
+    correct=["A", 'C', 'A', 'A', 'D', 'B', 'C', 'A', 'C', 'B', 'A', 'D', 'C', 'A', 'D', 'C', 'B', 'B', 'D', 'A']
     
+    student=open('Test.txt', 'r')
+    #in a loop student.append()
+    
+    x=0
+    while len(correct)== len(student) and x<=20:
+        if correct[x]==student[x]:
+            x +=1
+            cor +=1
+        else:
+            x +=1
+               
+            
+             
     if cor < 15:
-        
+        print("You failed the test")
+    else:
+        print("Congratulations you passed with a score of", cor, "/20")    
 
 
