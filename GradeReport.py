bcf6d20529b709238ea3f95f2744b01b4f9bae0b
echang19 <43009998+echang19@users.noreply.github.com>
2019-03-14 09:54:57 -0400

    Add files via upload

---
+++ GradeReport.py
@@ -0,0 +1,10 @@
+'''
+Created on Mar 12, 2019
+
+@author: Evan A. Chang
+Grade Report
+'''
+
+studentlist={'Cooper':['81','86', '90', '97'],'Jennie':['98', '79','99', '87', '82'], 'Julia':['87', '80','75', '10', '78']}
+
+
