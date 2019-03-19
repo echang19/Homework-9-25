1991add25c5ed5e74c920aec9bd2414c3489d58a
echang19 <43009998+echang19@users.noreply.github.com>
2019-03-19 12:23:57 -0400

    Add files via upload

---
+++ studentRoster.py
@@ -0,0 +1,18 @@
+'''
+Created on Feb 14, 2019
+
+@author: Evan A. Chang
+
+Student Roster
+'''
+
+def main():
+    
+    
+def studentRoster(list):
+    print("when you are done your list type done")
+    list=[]
+    while list != "done"
+    list.append=input("please enter the name of the student")
+    
+    
