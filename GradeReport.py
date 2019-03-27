044bdd9c89e0d14554f6ace5885ca869165d1a91
echang19 <43009998+echang19@users.noreply.github.com>
2019-03-27 12:13:32 -0400

    Add files via upload

--- GradeReport.py
+++ GradeReport.py
@@ -4,7 +4,16 @@ Created on Mar 12, 2019
 @author: Evan A. Chang
 Grade Report
 '''
-
-studentlist={'Cooper':['81','86', '90', '97'],'Jennie':['98', '79','99', '87', '82'], 'Julia':['87', '80','75', '10', '78']}
-
-
+def main():
+    studentList={'Cooper':['81','86', '90', '97'],'Jennie':['98', '79','99', '87', '82'], 'Julia':['87', '80','75', '10', '78']}
+    student=''
+    
+    while student.lower != 'done':
+        student= input("Please enter the name of a student")
+        student.str
+        grades=''
+        while grades.lower !='done':
+            grades=input('please enter the students grades when done type "done" ')
+            grades.str
+            studentList[student]=grades
+    
