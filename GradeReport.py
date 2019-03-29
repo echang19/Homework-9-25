216bce2523379ce74f0fcbe5841e8a08eb5cfbf6
echang19 <43009998+echang19@users.noreply.github.com>
2019-03-29 12:45:43 -0400

    Add files via upload

--- GradeReport.py
+++ GradeReport.py
@@ -7,8 +7,9 @@ Grade Report
 def main():
     studentList={'Cooper':['81','86', '90', '97'],'Jennie':['98', '79','99', '87', '82'], 'Julia':['87', '80','75', '10', '78']}
     student=''
-    
-    while student.lower != 'done':
+    read=input("Would you like to access a student's grades?")
+    read.lower()
+    if read== "no":
         student= input("Please enter the name of a student")
         student.str
         grades=''
@@ -16,4 +17,11 @@ def main():
             grades=input('please enter the students grades when done type "done" ')
             grades.str
             studentList[student]=grades
+    elif read=="yes":
+        name=input("Please enter the name of the student you want to see")
+        print(studentList[name])
+        again=input("would you like to see another's students grades?")
+        while again.lower()=='yes':
+            name=input("Please enter the name of the student you want to see")
+            print(studentList[name])
     
