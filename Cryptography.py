20f930930e43058ce4f47403f5448a17868d452d
echang19 <43009998+echang19@users.noreply.github.com>
2019-04-10 12:43:35 -0400

    Add files via upload

--- Cryptography.py
+++ Cryptography.py
@@ -2,22 +2,37 @@
 Created on Apr 2, 2019
 
 @author: Evan A. Chang
-Cryptography
+Cryptography part 1
 '''
-def cypher.py():
-    
+def main():    
     text=input("please enter a text file you want to decode")
     encryption=open(text,'r')
-    code1=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',"A",'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    code2=[]
+    alpha=[]
+    import string
+    for c in string.ascii_lowercase:
+        alpha.append(c)
+    
+    code=[]
     x=0
     #new encryption
     crypt=int(input('How many spaces would would you care to shift your alphabet'))
-    for i in range(0, 52):
-        code2.append=code1[(x+crypt)%52]
+    for i in range(0, 26):
+        code.append=alpha[(x+crypt)%26]
         x+=1
     t=0
-    for i in range text
+    new=0
+    newEncryption=''
+    for line in encryption:
+        if encryption[t] !=alpha[new%26]:
+            new+=1
+        elif encryption[t]==alpha[new]:
+            newEncryption.append=code[new]
+            t+=1
+        if encryption[t].isupper():
+            
+            
+        
+        
     
     
     
