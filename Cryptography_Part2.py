fe6e6181590a6b4631ee114eafd37d3daabd4956
echang19 <43009998+echang19@users.noreply.github.com>
2019-04-29 12:45:19 -0400

    Add files via upload

--- Cryptography_Part2.py
+++ Cryptography_Part2.py
@@ -5,46 +5,85 @@ Created on Apr 2, 2019
 Cryptography part 1
 '''
 def caesar_cypher(fname, encrypt, shift):
-        
-    fname=input("please enter a text file you want to decode")
-    encryption=open('fname.txt','r')
-    newtext=encryption.read()
+    enc=input("Would you like to encrypt or decrypt a file?")
+    if enc.lower()=="encrypt":
+        fname=input("please enter a text file you want to encode")
+        encryption=open('fname.txt','r')
+        newtext=encryption.read()
    
-    alpha=[]
-    import string
-    for c in string.ascii_lowercase:
-        alpha.append(c)
+        alpha=[]
+        import string
+        for c in string.ascii_lowercase:
+            alpha.append(c)
     
-    code=[]
-    x=0
+            code=[]
+            x=0
     #new encryption
-
-    for i in range(0, 26):
-        code.append=alpha[(x+3)%26]
-        x+=1
-    t=0
-    new=0
-    newEncryption=''
-    for line in encryption:
-        if encryption[t] !=code[new%26]:
+        count=int(input("how many spaces do you want to shift the alphabet to code"))
+        for i in range(0, 26):
+            code.append=alpha[(x+count)%26]
+            x+=1
+        t=0
+        new=0
+        newEncryption=''
+        for line in encryption:
+            if encryption[t] !=code[new%26]:
             new+=1
-        elif encryption[t]==code[new]:
+            elif encryption[t]==code[new]:
             newEncryption.append=code[new],sep=''
             t+=1
-        elif encryption[t].isupper():
+            elif encryption[t].isupper():
             code.upper()
-        elif encryption[t].islower():
+            elif encryption[t].islower():
             code.lower()
-    length=len(newEncryption)
-    print(newEncryption)
-    Test_ENC=open('newEncryption','w')
+            length=len(newEncryption)
+            print(newEncryption)
+            Test_ENC=open('newEncryption','w')
+            for i in length:
+                Test_ENC.write('newEncryption')
+                Test_ENC.close()
+    
+    #Decryption
+    elif enc.lower()=="decrypt":
+        fname=input("please enter a text file you want to decode")
+        encryption=open('fname.txt','r')
+        newtext=encryption.read()
+        
+        alpha=[]
+        import string
+        for c in string.ascii_lowercase:
+            alpha.append(c)
+        code=[]
+        x=0
+    #new decryption
+        count=int(input("how many spaces do you want to shift the alphabet to code"))
+        for i in range(0, 26):
+            code.append=alpha[(x+count)%26]
+            x+=1
+        t=0
+        new=0
+        newDecryption=''
+        for line in encryption:
+            if encryption[t] !=code[new%26]:
+                new+=1
+            elif encryption[t]==code[new]:
+                newEncryption.append=code[new],sep=''
+                t+=1
+            elif encryption[t].isupper():
+                code.upper()
+            elif encryption[t].islower():
+                code.lower()
+        length=len(newDecryption)
+        print(newDecryption)
+    Test_DEC=open('newDecryption','w')
     for i in length:
-        Test_ENC.write('newEncryption')
-    Test_ENC.close()
-    print(newEncryption)
+        Test_DEC.write('newDecryption')
+    Test_DEC.close()
+    print(newDecryption)
+
             
             
             
         
         
-caesar()   
+caesar_cypher()   
